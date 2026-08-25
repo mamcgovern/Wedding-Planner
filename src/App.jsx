45c7601745052df55d7c5843f95d693511c9440e
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-08-25 11:17:03 -0500

    Base app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import AppLayout from "./components/layout/AppLayout";
+import AppLayout from "./components/layout/AppLayout.jsx";
 
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
